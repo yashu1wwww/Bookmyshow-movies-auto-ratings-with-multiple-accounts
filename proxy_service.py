bf9817c3069c530704fff4a947bf7c22b2c21c56
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-10-12 10:49:20 +0530

    Update proxy_service.py

--- proxy_service.py
+++ proxy_service.py
@@ -1,3 +1,3 @@
 Config = {
-    "proxy_server" : "input your proxy server"  #213.168.210.76 sample proxy server to prevent instagram's ip ban
+    "proxy_server" : "input your proxy server"  #213.168.210.76 sample proxy server to by pass location..
 }
