9b20b2ff3dcc4cd3132a8b3565073bec96f30db2
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-10-20 08:02:47 +0530

    Update rate.py

--- rate.py
+++ rate.py
@@ -14,6 +14,12 @@ with open("urls.txt") as f:
     for url in f:
         driver.get(url)  
         
+driver.get("https://in.bookmyshow.com/bengaluru/movies/master/ET00110368") if url not runninf from using with open("urls.txt") as f: use these by removing # in starting and add # to like these 
+  
+#with open("urls.txt") as f:
+    #for url in f:
+        #driver.get(url)  
+
 driver.implicitly_wait(6)
 
 driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/section[1]/div/div/div[2]/section[2]/div[1]/button').click()
